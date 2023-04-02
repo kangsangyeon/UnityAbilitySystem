14c8f5b1588b1fd6c256b3b866ff81e408fda0b5
sanyoni <ksyline1997@gmail.com>
2023-04-02 21:08:22 +0900

    02. StatController

---
+++ Assets/@MyProject/StatSystem/Scripts/Runtime/StatController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace StatSystem
+{
+    public class StatController : MonoBehaviour
+    {
+        [SerializeField] private StatDatabase m_StatDatabase;
+        protected Dictionary<string, Stat> m_Stats = new Dictionary<string, Stat>(StringComparer.OrdinalIgnoreCase);
+        private bool m_IsInitialized;
+
+        public Dictionary<string, Stat> stats => m_Stats;
+        public bool IsInitialized() => m_IsInitialized;
+        public UnityAction initialized;
+        public UnityAction willUninitialize;
+
+        private void Awake()
+        {
+            if (m_IsInitialized == false)
+            {
+                m_IsInitialized = true;
+                Initialize();
+                initialized.Invoke();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            willUninitialize.Invoke();
+        }
+
+        private void Initialize()
+        {
+            foreach (var _definition in m_StatDatabase.stats)
+            {
+                m_Stats.Add(_definition.name, new Stat(_definition));
+            }
+        }
+    }
+}
