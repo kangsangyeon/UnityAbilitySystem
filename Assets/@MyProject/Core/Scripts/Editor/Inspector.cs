6069a6e39b8d1f7a8807f164824ce4436bfcaca3
sanyoni <ksyline1997@gmail.com>
2023-04-03 10:30:02 +0900

    08. ScriptableObject Collection Editor

---
+++ Assets/@MyProject/Core/Scripts/Editor/Inspector.cs
@@ -0,0 +1,12 @@
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Core.Editor
+{
+    public class Inspector : InspectorElement
+    {
+        public new class UxmlFactory : UxmlFactory<Inspector, UxmlTraits>
+        {
+        }
+    }
+}
---
+++ Assets/@MyProject/Core/Scripts/Editor/SplitView.cs
@@ -0,0 +1,11 @@
+using UnityEngine.UIElements;
+
+namespace Core.Editor
+{
+    public class SplitView : TwoPaneSplitView
+    {
+        public new class UxmlFactory : UxmlFactory<SplitView, UxmlTraits>
+        {
+        }
+    }
+}
