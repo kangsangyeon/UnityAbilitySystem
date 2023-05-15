099d68408a5f2306c1c86fed4a61eb37b23f3768
DESKTOP-KSANGYE\KOCCA21 <ksyline1997@gmail.com>
2023-05-15 14:05:26 +0900

    39. Physical Defense Tests

--- Assets/@MyProject/StatSystem/Tests/Runtime/AttributeTests.cs
+++ Assets/@MyProject/StatSystem/Tests/Runtime/AttributeTests.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Core;
 using NUnit.Framework;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -16,7 +19,7 @@ namespace StatSystem.Tests
                 "Assets/@MyProject/StatSystem/Tests/Scenes/Test.unity",
                 new LoadSceneParameters(LoadSceneMode.Single, LocalPhysicsMode.None));
         }
-        
+
         [UnityTest]
         public IEnumerator Attribute_WhenModifierApplied_DoesNotExceedCap()
         {
@@ -34,7 +37,7 @@ namespace StatSystem.Tests
             Assert.AreEqual(100, _health.currentValue);
             Assert.AreEqual(100, _health.value);
         }
-        
+
         [UnityTest]
         public IEnumerator Attribute_WhenModifierApplied_DoesNotGoBelowZero()
         {
@@ -51,6 +54,30 @@ namespace StatSystem.Tests
             });
             Assert.AreEqual(0, _health.currentValue);
             Assert.AreEqual(100, _health.value);
+        }
+
+        [UnityTest]
+        public IEnumerator Attribute_WhenTakeDamage_DamageReducedByDefense()
+        {
+            yield return null;
+
+            StatController _statController = GameObject.FindObjectOfType<StatController>();
+            Health _health = _statController.stats["Health"] as Health;
+            Assert.AreEqual(100, _health.currentValue);
+            _health.ApplyModifier(new HealthModifier()
+            {
+                magnitude = -10,
+                type = ModifierOperationType.Additive,
+                isCriticalHit = false,
+                source = ScriptableObject.CreateInstance<Ability>()
+            });
+            Assert.AreEqual(95, _health.currentValue);
+        }
+
+        private class Ability : ScriptableObject, ITaggable
+        {
+            private List<string> m_Tags = new List<string>() { "Physical" };
+            public ReadOnlyCollection<string> tags => m_Tags.AsReadOnly();
         }
     }
 }
