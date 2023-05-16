06f1ce8818b4a64fb2b6a60a16bc213657fd7737
sanyoni <ksyline1997@gmail.com>
2023-05-17 00:06:57 +0900

    45. Gameplay Effect Tests

---
+++ Assets/@MyProject/AbilitySystem/Tests/Scripts/Runtime/GameplayEffectControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using AbilitySystem;
+using NUnit.Framework;
+using StatSystem;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace MyProject.AbilitySystem.Tests.Scripts.Runtime
+{
+    public class GameplayEffectControllerTests
+    {
+        private GameObject m_PlayerPrefab;
+        private GameObject m_EnemyPrefab;
+        private GameObject m_Player;
+        private GameObject m_Enemy;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            m_PlayerPrefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>(
+                    "Assets/@MyProject/AbilitySystem/Tests/Prefabs/Player.prefab");
+
+            m_EnemyPrefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>(
+                    "Assets/@MyProject/AbilitySystem/Tests/Prefabs/Enemy.prefab");
+        }
+
+        [SetUp]
+        public void BeforeEachTestSetup()
+        {
+            m_Player = GameObject.Instantiate(m_PlayerPrefab);
+            m_Enemy = GameObject.Instantiate(m_EnemyPrefab);
+        }
+
+        [UnityTest]
+        public IEnumerator GameplayEffectController_WhenEffectApplied_ModifyAttribute()
+        {
+            yield return null;
+            GameplayEffectController _effectController = m_Player.GetComponent<GameplayEffectController>();
+            StatController _statController = m_Player.GetComponent<StatController>();
+            GameplayEffectDefinition _damageEffectDefinition =
+                AssetDatabase.LoadAssetAtPath<GameplayEffectDefinition>(
+                    "Assets/@MyProject/AbilitySystem/Tests/ScriptableObjects/Test_GameplayEffect_HealthModifier.asset");
+            GameplayEffect _damageEffect = new GameplayEffect(_damageEffectDefinition, null, m_Enemy);
+            Health _health = _statController.stats["Health"] as Health;
+            Assert.AreEqual(100, _health.currentValue);
+            _effectController.ApplyGameplayEffectToSelf(_damageEffect);
+            Assert.AreEqual(90, _health.currentValue);
+        }
+    }
+}
