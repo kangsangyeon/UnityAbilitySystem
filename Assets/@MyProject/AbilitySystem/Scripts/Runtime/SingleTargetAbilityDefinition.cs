5d7eb45b21eca03f2606ebfc81e967a4a8deb1b2
DESKTOP-KSANGYE\KOCCA21 <ksyline1997@gmail.com>
2023-05-18 11:31:28 +0900

    fix: SingleTargetAbilityDefinition에 AbilityType Attribute이 누락된 것을 수정합니다.

--- Assets/@MyProject/AbilitySystem/Scripts/Runtime/SingleTargetAbilityDefinition.cs
+++ Assets/@MyProject/AbilitySystem/Scripts/Runtime/SingleTargetAbilityDefinition.cs
@@ -5,6 +5,7 @@ namespace AbilitySystem
     /// <summary>
     /// 단일 entity를 대상으로 적용할 수 있는 ability를 정의하는 scriptable object입니다.
     /// </summary>
+    [AbilityType(typeof(SingleTargetAbility))]
     [CreateAssetMenu(fileName = "SingleTargetAbility", menuName = "AbilitySystem/Ability/SingleTargetAbility", order = 0)]
     public class SingleTargetAbilityDefinition : ActiveAbilityDefinition
     {
