4d43a2861455ee6b64ca7a2932fcfecd9b5bc6ed
DESKTOP-KSANGYE\KOCCA21 <ksyline1997@gmail.com>
2023-06-05 17:18:31 +0900

    73. Ability Element

---
+++ Assets/@MyProject/AbilitySystem/Scripts/Runtime/UI/AbilityElement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AbilitySystem.UI
+{
+    public class AbilityElement : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<AbilityElement, UxmlTraits>
+        {
+        }
+
+        public AbilityElement()
+        {
+            var _visualTree = Resources.Load<VisualTreeAsset>("UI/Ability");
+            _visualTree.CloneTree(this);
+        }
+    }
+}
