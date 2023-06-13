f45509f0c5c07eda46f604023e1579eafc0336af
DESKTOP-KSANGYE\KOCCA21 <ksyline1997@gmail.com>
2023-06-13 11:42:03 +0900

    fix: health attribute에 source 없는 modifier를 적용할 때 오류가 발생하는 문제를 수정합니다.

--- Assets/@MyProject/StatSystem/Scripts/Runtime/Health.cs
+++ Assets/@MyProject/StatSystem/Scripts/Runtime/Health.cs
@@ -16,16 +16,16 @@ namespace StatSystem
         public override void ApplyModifier(StatModifier _modifier)
         {
             ITaggable _source = _modifier.source as ITaggable;
-
-            if (_source.tags.Contains("healing")
-                && m_TagController.Contains("zombify"))
-            {
-                // zombify 상태에서 healing을 받으면 힐을 받는 양이 그대로 대미지로 들어옵니다.
-                _modifier.magnitude *= -1;
-            }
-
+            
             if (_source != null)
             {
+                if (_source.tags.Contains("healing")
+                    && m_TagController.Contains("zombify"))
+                {
+                    // zombify 상태에서 healing을 받으면 힐을 받는 양이 그대로 대미지로 들어옵니다.
+                    _modifier.magnitude *= -1;
+                }
+                
                 if (_source.tags.Contains("Pure"))
                 {
                     // 아무것도 하지 않습니다.
